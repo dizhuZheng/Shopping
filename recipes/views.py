e11119385c8c5ad9c9f9366fe7c3cc5e3476587e
dizhu <dizhu210@gmail.com>
2019-07-11 17:51:36 -0400

    now nav bar works

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from .models import Image
+from .models import *
+
 # Create your views here.
+logo = 'Welcome to my recipe'
 def index(request):
     """The home page for recipes"""
-    return render(request, 'recipes/index.html')
+    return render(request, 'recipes/index.html', {'logo': logo})
