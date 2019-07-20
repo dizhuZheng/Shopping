1036e5a20b7fd12fe0e3a5b8aaa1a944abcabab0
dizhu <dizhu210@gmail.com>
2019-07-19 20:28:10 -0400

    login logout register work

--- users/views.py
+++ users/views.py
@@ -4,5 +4,28 @@ from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse
 from django.shortcuts import render
 
-
 # Create your views here.
+
+def logout_view(request):
+    """log the user out."""
+    logout(request)
+    return HttpResponseRedirect(reverse('recipes:index'))
+
+
+def register(request):
+    """register new user"""
+    if request.method != 'POST':
+        form = UserCreationForm()
+    else:
+        #process completed form.
+        form = UserCreationForm(data=request.POST)
+
+        if form.is_valid():
+            new_user = form.save()
+            #log the user in and then redirect to home page
+            authenticated_user = authenticate(username=new_user.username,
+            password=request.POST['password1'])
+            login(request, authenticated_user)
+            return HttpResponseRedirect(reverse('recipes:index'))
+    context = {'form': form}
+    return render(request, 'users/register.html', context)
