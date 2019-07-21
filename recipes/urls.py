c3ce82cf831ff221bf243648df8f6b8126eb1dea
dizhu <dizhu210@gmail.com>
2019-07-20 20:15:36 -0400

    add a menu page

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
-
-from .views import index
+from .views import index, dishes, dish
 
 urlpatterns = [
     #Home page
     url(r'^$', index, name='index'),
+    url(r'^dishes/$', dishes, name='dishes'),
+    url(r'^dishes/(?P<dish_id>\d+)/$', dish, name='dish'),
+
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect, Http404
 from .models import *
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 logo = 'Welcome to my recipe'
 def index(request):
     """The home page for recipes"""
     return render(request, 'recipes/index.html', {'logo': logo})
+
+
+@login_required
+def dishes(request):
+    """show all dishes"""
+    dishes = Dish.objects.all()
+    context = {'dishes': dishes}
+    return render(request, 'recipes/dishes.html', context)
+
+
+@login_required
+def dish(request, dish_id):
+    """show a single topic and all its entries"""
+    dish = Dish.objects.get(id=dish_id)
+    context = {'dish': dish}
+    return render(request, 'recipes/dish.html', context)
