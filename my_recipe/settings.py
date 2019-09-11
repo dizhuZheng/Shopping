c8c759eaf20a85bd47502733a7de9cd26c88e612
dizhu <dizhu210@gmail.com>
2019-09-11 19:40:01 -0400

    can log out

--- my_recipe/settings.py
+++ my_recipe/settings.py
@@ -148,7 +148,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media').replace('\\', '/')
 LOGIN_REDIRECT_URL = '/accounts/profile/'
-LOGOUT_REDIRECT_URL = '/'
+ACCOUNT_LOGOUT_ON_GET = True
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
