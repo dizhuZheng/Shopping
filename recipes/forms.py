0d2b3edc5130be0c822fb08d527a63cf9f32bfbc
dizhu <dizhu210@gmail.com>
2019-09-20 21:16:18 -0400

    modify usr dropdown layout

--- recipes/forms.py
+++ recipes/forms.py
@@ -1,7 +1,19 @@
-from django import forms
+from django.forms import ModelForm
 from .models import Dish
 
-class DishForm(forms.ModelForm):
+class DishForm(ModelForm):
     class Meta:
         model = Dish
         fields = ['name', 'owner', 'rating', 'difficulty']
+
+# class EntryForm(ModelForm):
+#     class Meta:
+#         model = Entry
+#         fields = "__all__"
+#         exclude = None
+#         widgets = {'directions': forms.Textarea(attrs={'cols':80})}
+#         error_messages = {
+#             'name':{'requried': 'dish name can\'t be null '},
+#             'ingredients':{'requried': 'ingredients can\'t be null '},
+#             'directions':{'requried': 'directions can\'t be null '},
+#         }
