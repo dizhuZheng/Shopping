2335144cb6bfb9bb0593b12c75bcdd7d43cd7fd9
dizhu <dizhu210@gmail.com>
2019-09-25 22:49:42 -0400

    recipe model

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from recipes.models import Category
+from recipes.models import Category, Flavor, Ingredient, Sauce
 
 # Register your models here.
 admin.site.register(Category)
+admin.site.register(Flavor)
+admin.site.register(Ingredient)
+admin.site.register(Sauce)
--- recipes/models.py
+++ recipes/models.py
@@ -14,3 +14,45 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+class Flavor(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=200)
+    amount = models.SmallIntegerField()
+
+    def __str__(self):
+        return '%s,%d' % (name, amount)
+
+class Sauce(models.Model):
+    name = models.CharField(max_length=200)
+    amount = models.SmallIntegerField()
+
+    def __str__(self):
+        return '%s,%d' % (name, amount)
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=100)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    flavor = models.ManyToManyField(Flavor, blank=True, default='')
+    difficulty = models.IntegerField()
+    rating = models.IntegerField()
+    cooking_time = models.IntegerField()
+    PEOPLE_CHOICES = (
+        (1, '1 person'),
+        (2, '2 people'),
+        (3, '3 people'),
+        (4, 'More')
+    )
+    people = models.IntegerField(max_length=2, choices=PEOPLE_CHOICES, default=1)
+    ingredient = models.ManyToManyField(Ingredient, help_text='such as; rice')
+    sauce = models.ManyToManyField(Ingredient, help_text='such as: slat')
+    cover_img = models.ImageField(default='')
+    date_added = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return '%s,%d' % (name, amount)
